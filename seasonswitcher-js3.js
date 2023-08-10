f5eac31720ab8f4baafd5689fc327d96585ac721
sravanthithatipally <126408578+sravanthithatipally@users.noreply.github.com>
2023-08-11 01:52:46 +0530

    Add files via upload

---
+++ seasonswitcher-js3.js
@@ -0,0 +1,23 @@
+let xsandsimage = document.getElementById("seasonSmallImage");
+let mtoxlimage = document.getElementById("seasonMediumImage");
+
+function springclicked() {
+    xsandsimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    mtoxlimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+}
+
+function summerclicked() {
+    xsandsimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+    mtoxlimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+}
+
+function autumnclicked() {
+    xsandsimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+    mtoxlimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+}
+
+function winterclicked() {
+    xsandsimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+    mtoxlimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+
+}
